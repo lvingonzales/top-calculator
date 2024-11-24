423ea0f31f3e2bc5cb9931a612c8215bd1b60b91
lvingonzales <lvingonzales15@gmail.com>
2024-11-24 12:41:12 -0500

    Abandon multi-file and module idea for now

--- main.js
+++ main.js
@@ -1,38 +1,81 @@
+const display = document.querySelector("#display");
+const displayText = document.createElement("p");
+displayText.setAttribute("class", "display-text");
+displayText.textContent = 'TEST';
+display.appendChild(displayText);
 
+
+const digitsPanel = document.querySelector("#digits-panel");
+let buttons = new Array(10);
+
+// Quickly creates the basic 0-9 digits
+for (let i =0; i < buttons.length; i++){
+    buttons[i] = document.createElement("button");
+    buttons[i].setAttribute("class", "buttons digits")
+    buttons[i].textContent = i;
+    digitsPanel.appendChild(buttons[i]);
+}
+
+// Adds the period and backspace key
+buttons.push(document.createElement("button"));
+buttons[10].setAttribute("class", "buttons digits");
+buttons[10].textContent = '.';
+digitsPanel.appendChild(buttons[10]);
+
+buttons.push(document.createElement("button"));
+buttons[11].setAttribute("class", "buttons digits");
+buttons[11].textContent = '<-';
+digitsPanel.appendChild(buttons[11]);
+
+let input;
+for (let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", () =>{
+        input = buttons[i].textContent;
+        handleInput(input);
+    })
+}
+
+
+const operandsPanel = document.querySelector("#operands-panel");
+
+
+let inputArray = [];
 let equation;
 let operandArray;
 let numberArray;
 let result;
 
-equation = prompt("enter an equation");
-handleInput();
+function handleInput(input){
 
-function handleInput(){
+    inputArray.push(input);
 
-    equation = equation.split("");
+    console.log(inputArray);
 
-    numberArray = equation.filter((x) => x >= 0);
-    operandArray = equation.filter((x) => x === '+' || x === '-' || x === '*' || x === '/');
 
-    for (let i = 0; i < operandArray.length; i++){
-        switch (operandArray[i]){
-            case '+':
-                addFunc();
-                break;
-            case '-':
-                subtractFunc();
-                break;
-            case '*':
-                multiplyFunc();
-                break;
-            case '/':
-                divideFunc();
-                break;
-            default:
-                alert("ERROR");
-                break;
-        }
-    }
+    // equation = equation.split("");
+
+    // numberArray = equation.filter((x) => x >= 0);
+    // operandArray = equation.filter((x) => x === '+' || x === '-' || x === '*' || x === '/');
+
+    // for (let i = 0; i < operandArray.length; i++){
+    //     switch (operandArray[i]){
+    //         case '+':
+    //             addFunc();
+    //             break;
+    //         case '-':
+    //             subtractFunc();
+    //             break;
+    //         case '*':
+    //             multiplyFunc();
+    //             break;
+    //         case '/':
+    //             divideFunc();
+    //             break;
+    //         default:
+    //             alert("ERROR");
+    //             break;
+    //     }
+    // }
 }
 
 function addFunc () {
