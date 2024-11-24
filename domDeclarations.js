75ad347c38e776767a9f84fb9e6b29948041664a
lvingonzales <lvingonzales15@gmail.com>
2024-11-24 12:10:00 -0500

    Add buttons on the Digits Panel

--- domDeclarations.js
+++ domDeclarations.js
@@ -2,4 +2,21 @@ const display = document.querySelector("#display");
 const digitsPanel = document.querySelector("#digits-panel");
 const operandsPanel = document.querySelector("#operands-panel");
 
+let buttons = new Array(10);
 
+for (let i =0; i < buttons.length; i++){
+    buttons[i] = document.createElement("button");
+    buttons[i].setAttribute("class", "buttons digits")
+    buttons[i].textContent = i;
+    digitsPanel.appendChild(buttons[i]);
+}
+
+buttons.push(document.createElement("button"));
+buttons[10].setAttribute("class", "buttons digits");
+buttons[10].textContent = '.';
+digitsPanel.appendChild(buttons[10]);
+
+buttons.push(document.createElement("button"));
+buttons[11].setAttribute("class", "buttons digits");
+buttons[11].textContent = '<-';
+digitsPanel.appendChild(buttons[11]);
